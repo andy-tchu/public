0ec3dfb8ef1be31bc9ffa1e110441af83d02deb4
Andrey Chuchalov <andy.tchu@gmail.com>
2022-11-22 16:24:15 +0300

    WebApp

--- WebApp/WebApp.py
+++ WebApp/WebApp.py
@@ -23,7 +23,12 @@ async def getAllItems(request):
 #обрабатывает GET-запрос на получение всех магазинов
 @routes.get('/getAllStores/')
 async def getAllStores(request):
-    return web.Response(text="Hello, stores")
+    pool = request.app['pool']
+    async with pool.acquire() as connection:
+        async with connection.transaction():
+            result = await connection.fetch('SELECT * FROM store')
+            result_as_dict: List[Dict] = [dict(item) for item in result]
+            return web.json_response(result_as_dict)
 
 #обрабатывает POST-запрос с json-телом для сохранения данных о произведенной продаже (id товара + id магазина)
 @routes.post('/postSale/')
@@ -33,12 +38,22 @@ async def postSale(request):
 #обрабатывает GET-запрос на получение данных по топ 10 самых доходных магазинов за месяц (id + адреса + суммарная выручка)
 @routes.get('/getTopStoresForMonth/')
 async def getTopStoresForMonth(request):
-    return web.Response(text="Hello, TopStoresForMonth")
+    pool = request.app['pool']
+    async with pool.acquire() as connection:
+        async with connection.transaction():
+            result = await connection.fetch('SELECT st.id, st.address, sum(coalesce(i.price,0)) FROM store st LEFT OUTER JOIN sales s ON s.store_id = st.id LEFT OUTER JOIN item i ON s.item_id = i.id GROUP BY st.id, st.address ORDER BY sum(coalesce(i.price,0)) DESC fetch first 10 rows only')
+            result_as_dict: List[Dict] = [dict(item) for item in result]
+            return web.json_response(result_as_dict)
 
 #обрабатывает GET-запрос на получение данных по топ 10 самых продаваемых товаров (id + наименование + количество проданных товаров)
 @routes.get('/getTenBestSellItems/')
 async def getTenBestSellItems(request):
-    return web.Response(text="Hello, TenBestSellItems")
+    pool = request.app['pool']
+    async with pool.acquire() as connection:
+        async with connection.transaction():
+            result = await connection.fetch('SELECT i.id, i.name, count(coalesce(i.price,0)) FROM item i LEFT OUTER JOIN sales s ON s.item_id = i.id LEFT OUTER JOIN store st ON s.store_id = st.id GROUP BY i.id, i.name ORDER BY count(coalesce(i.price,0)) DESC fetch first 10 rows only')
+            result_as_dict: List[Dict] = [dict(item) for item in result]
+            return web.json_response(result_as_dict)
 
 async def create_database_pool(app: Application):
     app['pool'] = await asyncpg.create_pool(user=DB_USER, password=DB_PASSWORD,
